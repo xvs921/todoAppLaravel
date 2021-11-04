eb023b3036ef3649c5e419171c3dcaa7949587f6
Vorák Gergő <vorakgeri@gmail.com>
2021-11-04 21:24:59 +0100

    method patch

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,14 +4,15 @@
 <h1>Edit Task</h1>
 
 <form method="POST" action="/tasks/update">
-  @csrf
-  <div class="form-group">
-    <label for="description">Task Description</label>
-    <input class="form-control" name="description" value="{{ $task->description }}"/>
-    <label>Task Created At: {{ $task->created_at}}</label>
-  </div>
-  <div class="form-group">
-    <button type="submit" class="btn btn-primary">Edit Task</button>
-  </div>
+    @method('PATCH') 
+    @csrf
+    <div class="form-group">
+      <label for="description">Task Description</label>
+      <input class="form-control" name="description" value="{{ $task->description }}"/>
+      <label>Task Created At: {{ $task->created_at}}</label>
+    </div>
+    <div class="form-group">
+      <button type="submit" class="btn btn-primary">Edit Task</button>
+    </div>
 </form>
 @endsection
