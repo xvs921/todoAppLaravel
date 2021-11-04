1a5d277b405b33f6fbdc47a0654fb713dc3ee0d8
Vorák Gergő <vorakgeri@gmail.com>
2021-11-04 12:15:01 +0100

    route add

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\TasksController@index');
 Route::get('/tasks', 'App\Http\Controllers\TasksController@index');
 Route::get('/tasks/create', 'App\Http\Controllers\TasksController@create');
+Route::get('/tasks/ready', 'App\Http\Controllers\TasksController@ready');
+Route::get('/tasks/todo', 'App\Http\Controllers\TasksController@todo');
 Route::post('/tasks', 'App\Http\Controllers\TasksController@store');
 
 Route::patch('/tasks/{id}', 'App\Http\Controllers\TasksController@update');
