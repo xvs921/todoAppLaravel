098119fdcf2fa6fb711d113dc46565c9925b8146
Vorák Gergő <vorakgeri@gmail.com>
2021-11-04 12:04:10 +0100

    delete and modify button

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,12 +3,27 @@
 @section('content')
     <h1>Hello Task app</h1>
 
+    <div class='testimonials'>
     @foreach($tasks as $t)
-        <div>{{ $t->description }}</div>
-        <form action="/tasks/{{ $t->id }}" method="POST">
-            @method('PATCH')
-            <button class="btn btn-light btn-block" input="submit">Complete</button>
-        </form>
+        <div class="card">
+			<div class="layer"></div>
+			<div class="content">
+				<p>{{ $t->description }}</p>
+                <form action="/tasks/{{ $t->id }}" method="GET">
+                    @method('GET')
+                    <button class="btn btn-error btn-block" input="submit">Modify</button>
+                </form>
+                <form action="/tasks/{{ $t->id }}" method="POST">
+                    @method('PATCH')
+                    <button class="btn btn-light btn-block" input="submit">Complete</button>
+                </form>
+                <form action="/tasks/{{ $t->id }}" method="DELETE">
+                    @method('DELETE')
+                    <button class="btn btn-error btn-block" input="submit">Delete</button>
+                </form>
+            </div>
+		</div>
     @endforeach
+    </div>
     <a href='tasks/create' class='btn btn-primary btn-lg btn-block'>new</a>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\TasksController@index');
-//Route::get('/tasks', 'App\Http\Controllers\TasksController@index');
+Route::get('/tasks', 'App\Http\Controllers\TasksController@index');
 Route::get('/tasks/create', 'App\Http\Controllers\TasksController@create');
 Route::post('/tasks', 'App\Http\Controllers\TasksController@store');
 
 Route::patch('/tasks/{id}', 'App\Http\Controllers\TasksController@update');
+
+Route::delete('/tasks/{id}', 'TasksController@delete');
