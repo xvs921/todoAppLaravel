3aa090e23ea85613930fa59c195bb04e26136e51
Vorák Gergő <vorakgeri@gmail.com>
2021-11-04 12:08:12 +0100

    card design

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,4 +1,76 @@
 @extends('base')
+<style>
+.testimonials
+{
+	margin: 200px auto 100px;
+	display: grid;
+	grid-template-columns: repeat( auto-fit, minmax(350px, 1fr));
+	grid-gap: 20px;
+}
+.testimonials .card
+{
+	position: relative;
+	width: 350px;
+	margin: 0 auto;
+	background: #333;
+	padding: 20px;
+	box-sizing: border-box;
+	text-align: center;
+	box-shadow: 0 10px 40px rgba(0,0,0,.5);
+	overflow: hidden;
+}
+.testimonials .card .layer
+{
+	position: absolute;
+	top: calc(100% - 2px);
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: linear-gradient(#03a9f4, #e91ee3);
+	z-index: 1;
+	transition: 0.5s;
+}
+.testimonials .card:hover .layer
+{
+	top: 0;
+}
+.testimonials .card .content
+{
+	position: relative;
+	z-index: 2;
+}
+.testimonials .card .content p
+{
+	font-size: 18px;
+	line-height: 24px;
+	color: #fff;
+}
+.testimonials .card .content .image
+{
+	width: 100%;
+	heigth: 100%;
+	margin: 0 auto;
+	border-radius: 50%;
+	overflow: hidden;
+	border: 4px solid #fff;
+	box-shadow: 0 10px 40px rgba(0,0,0,.2);
+}
+.testimonials .card .content .details h2
+{
+	font-size: 18px;
+	color: #fff;
+}
+.testimonials .card .content .details h2 span
+{
+	color: #03a9f4;
+	font-size: 14px;
+	transition: 0.5s;
+}
+.testimonials .card:hover .content .details h2 span
+{
+	color: #fff;
+}
+</style>
 
 @section('content')
     <h1>Hello Task app</h1>
