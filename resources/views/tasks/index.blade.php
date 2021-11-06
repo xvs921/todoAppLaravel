82cd1bdf8e876d307c0230fa1c25bbf3d8f95f15
Vorák Gergő <vorakgeri@gmail.com>
2021-11-06 21:15:25 +0100

    test

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -61,7 +61,7 @@
 						@csrf
 						<button class="btn btn-danger btn-lg" input="submit"><i class="fa fa-trash"></i></button>
 					</form>
-						<button class="btn btn-info btn-lg open-modal" input="submit" value="{{ $t->id }}"><i class="fa fa-pencil"></i></button>
+						<button class="btn btn-info btn-lg" input="submit" value="{{ $t->id }}" data-toggle="modal" data-target="#exampleModalCenter"><i class="fa fa-pencil"></i></button>
 
 					<form action="/tasks/{{ $t->id }}" method="POST">
 						@method('PATCH')
@@ -77,99 +77,45 @@
     @endforeach
     </div>
 
-	<div class="modal fade" id="linkEditorModal" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title" id="linkEditorModalLabel">Link Editor</h4>
-                        </div>
-                        <div class="modal-body">
-                            <form id="modalFormData" name="modalFormData" class="form-horizontal" novalidate="">
-
-                                <div class="form-group">
-                                    <label for="inputLink" class="col-sm-2 control-label">Link</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" id="link" name="link"
-                                               placeholder="Enter URL" value="">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-sm-2 control-label">Description</label>
-                                    <div class="col-sm-10">
-                                        <input type="text" class="form-control" id="description" name="description"
-                                               placeholder="Enter Link Description" value="">
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" id="btn-save" value="add">Save changes
-                            </button>
-                            <input type="hidden" id="link_id" name="link_id" value="0">
-                        </div>
-                    </div>
-                </div>
-            </div>
+	<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-body">
+						<center><h2 class="modal-title" id="exampleModalLongTitle">Játékos hozzáadása</h2></center>
+						<form method="post">
+							<div class="row">
+								<div class="col-md-6">
+									<center><p class="modalSzoveg">Név</p></center>
+									<input class="form-control" type="text" name="input_nev" placeholder="név" value="<?php echo $_SESSION["felv_nev"]; ?>">
+								</div>
+								<div class="col-md-6">
+									<center><p class="modalSzoveg">Összpontszám</p></center>
+									<input class="form-control" type="text" name="input_osszPontszam" placeholder="1 - 99" value="<?php echo $_SESSION["felv_osszPontszam"]; ?>">
+								</div>
+								<div class="col-md-6">
+									<center><p class="modalSzoveg">Hárompontos</p></center>
+									<input class="form-control" type="text" name="input_3pontos" placeholder="1 - 99" value="<?php echo $_SESSION["felv_3pontos"]; ?>">
+								</div>
+								<div class="col-md-6">
+									<center><p class="modalSzoveg">Zsákolás</p></center>
+									<input class="form-control" type="text" name="input_zsakolas" placeholder="1 - 99" value="<?php echo $_SESSION["felv_zsakolas"]; ?>">
+								</div>
+								<div class="col-md-6">
+									<center><p class="modalSzoveg">Ár</p></center>
+									<input class="form-control" type="text" name="input_ar" placeholder="1 - 250 000" value="<?php echo $_SESSION["felv_ar"]; ?>">
+								</div>
+								<div class="col-md-6">
+									<center><p class="modalSzoveg">Kép</p></center>
+									<input class="form-control" type="text" name="input_kep" placeholder="*.png vagy *.jpg" value="<?php echo $_SESSION["felv_kep"]; ?>">
+								</div>
+							</div>
+							<center><p class="modalSzoveg">Minden adat kitöltése kötelező!</p></center>
+							<center><p class="modalSzoveg">A pontszámok 1 és 99 között kell legyenek!</p></center>
+							<input type="hidden" name="action" value="btnFelvetel">
+							<div style="text-align:right"><input type="submit" class="btn btn-primary" value="Felvétel" id="loginbtn"></div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>  
 @endsection
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script>
-jQuery(document).ready(function($){
-
-    jQuery('body').on('click', '.open-modal', function () {
-        var link_id = $(this).val();
-        $.get('tasks/' + link_id, function (data) {
-            jQuery('#link_id').val(data.id);
-            jQuery('#link').val(data.url);
-            jQuery('#description').val(data.description);
-			let a = this.$.linkEditorModal;
-            jQuery('#btn-save').val("update");
-            jQuery('#linkEditorModal').modal('show');
-        })
-    });
-
-    // Clicking the save button on the open modal for both CREATE and UPDATE
-    $("#btn-save").click(function (e) {
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': jQuery('meta[name="csrf-token"]').attr('content')
-            }
-        });
-        e.preventDefault();
-        var formData = {
-            url: jQuery('#link').val(),
-            description: jQuery('#description').val(),
-        };
-        var state = jQuery('#btn-save').val();
-        var type = "POST";
-        var link_id = jQuery('#link_id').val();
-        var ajaxurl = 'links';
-        if (state == "update") {
-            type = "PUT";
-            ajaxurl = 'links/' + link_id;
-        }
-        $.ajax({
-            type: type,
-            url: ajaxurl,
-            data: formData,
-            dataType: 'json',
-            success: function (data) {
-                var link = '' + data.id + '' + data.url + '' + data.description + '';
-                link += ' ';
-                link += '';
-                if (state == "add") {
-                    jQuery('#links-list').append(link);
-                } else {
-                    $("#link" + link_id).replaceWith(link);
-                }
-                jQuery('#modalFormData').trigger("reset");
-                jQuery('#linkEditorModal').modal('hide')
-            },
-            error: function (data) {
-                console.log('Error:', data);
-            }
-        });
-    });
-});
-</script>
