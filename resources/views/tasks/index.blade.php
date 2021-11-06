f37092a155f8b03ca089f3315550a2fd63724856
Vorák Gergő <vorakgeri@gmail.com>
2021-11-06 15:36:03 +0100

    test

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,7 +5,7 @@
 	width: 90%;
 	margin: 60px auto;
 	display: grid;
-	grid-template-columns: minmax(80%) 1fr;
+	grid-template-columns: repeat( auto-fit, minmax(400px, 1fr));
 	grid-gap: 20px;
 }
 .ready{
